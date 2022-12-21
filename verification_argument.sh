d5f4dbcc217e2b755d3b78af0bd0666d48b52c59
hadjuse <104326150+hadjuse@users.noreply.github.com>
2022-12-21 15:54:13 +0100

    Add files via upload
    Ajout des options des pays

--- verification_argument.sh
+++ verification_argument.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Traitement des options de la ligne de commande avec getops
-while getopts ":t:p:wmhFGSAHOQ" option
+while getopts ":t:p:wmhFGSAOQ" option
 do
     case "$option" in
         #traitement de la temperature accompagné d'une option 1 ou 2 ou 3
         t)
         MODE=$OPTARG
         if [[ $MODE -ne 1 && $MODE -ne 2 && $MODE -ne 3 ]]; then
-            echo "Les arguments sont compris entre 1 et 3 tout inclus/ Vous n'avez pas mis d'option"
+            echo "Les arguments sont compris entre 1 et 3 tout inclus ou Vous n'avez pas mis d'argument"
             exit 1
         fi
         echo "$MODE" 
@@ -16,7 +16,7 @@ do
         p) #traitement de la pression accompagné d'une option 1 ou 2 ou 3
         MODE=$OPTARG
         if [[ $MODE -ne 1 && $MODE -ne 2 && $MODE -ne 3 ]]; then
-            echo "Les arguments sont compris entre 1 et 3 tout inclus/ Vous n'avez pas mis d'options"
+            echo "Les arguments sont compris entre 1 et 3 tout inclus ou Vous n'avez pas mis d'argument"
             exit 1
         fi
         echo "$MODE"
@@ -40,6 +40,19 @@ do
         ;;
         S)
         echo "Saint-Pierre et Miquelon"
+        ;;
+        A)
+        echo "Antilles"
+        ;;
+        O)
+        echo "Océan indien"
+        ;;
+        Q)
+        echo "Antarctique"
+        ;;
+        *)
+        echo "$OPTARG: option invalide"
+        exit 1
         ;;
     esac
 done
