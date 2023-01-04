16f7c1e4d2c88d0418f2ddd9763eb8ed5677b529
hadjuse <104326150+hadjuse@users.noreply.github.com>
2023-01-04 09:55:07 +0100

    Add files via upload
    Finalisation des options geographiques

--- script_meteo.sh
+++ script_meteo.sh
@@ -21,34 +21,34 @@ test_options() {
 }
 #filtrage pour l'option -t
 filtrage_1() {
-    cut -d ';' -f 1,11,15 meteo_filtered_data_v1.csv >donnee_filtree_temperature_et_num_t.csv
+    awk -F';' '{print $10 ";" $11 ";" $1}' meteo_filtered_data_v1.csv>donnee_filtree_temperature_et_num_t.csv
 }
 filtrage_2() {
-    cut -d ';' -f 1,11,2,15 meteo_filtered_data_v1.csv >donnee_filtree_temperature_et_date_t.csv
+    awk -F';' '{print $10 ";" $1 ";" $11 ";" $2}' meteo_filtered_data_v1.csv>donnee_filtree_temperature_et_date_t.csv
 }
 filtrage_3() {
-    cut -d ';' -f 1,11,15 meteo_filtered_data_v1.csv >donnee_filtree_temperature_et_id_t.csv
+    awk -F';' '{print $10 ";" $1 ";" $11}' meteo_filtered_data_v1.csv>donnee_filtree_temperature_et_id_t.csv
+    #cut -d ';' -f 1,11,10 meteo_filtered_data_v1.csv >donnee_filtree_temperature_et_id_t.csv
 }
 #filtrage pour l'option -p
 filtrage_1_bis() {
-    cut -d ';' -f 11,15 meteo_filtered_data_v1.csv >donnee_filtree_temperature_et_num_p.csv
-
+    awk -F';' '{print $10 ";" $11 ";" $1}' meteo_filtered_data_v1.csv>donnee_filtree_temperature_et_num_p.csv
 }
 filtrage_2_bis() {
-    cut -d ';' -f 11,2,15 meteo_filtered_data_v1.csv >donnee_filtree_temperature_et_date_p.csv
+    awk -F';' '{print $10 ";" $2 ";" $11}' meteo_filtered_data_v1.csv>donnee_filtree_temperature_et_date_p
 }
 filtrage_3_bis() {
-    cut -d ';' -f 1,11,15 meteo_filtered_data_v1.csv >donnee_filtree_temperature_et_id_p.csv
+    awk -F';' '{print $10 ";" $11 ";" $1}' meteo_filtered_data_v1.csv>donnee_filtree_temperature_et_id_p.csv
 }
 #filtrage pour l'option -w
 filtrage_w() {
-    cut -d ';' -f 1,3,4,15 meteo_filtered_data_v1.csv >donnee_filtree_id_vent_moyenne.csv
+    awk -F';' '{print $10 ";" $1 ";" $3 ";" $4 ";" $10}' meteo_filtered_data_v1.csv>donnee_filtree_id_vent_moyenne.csv
 }
 filtrage_m() {
-    cut -d ';' -f 1,6,15 meteo_filtered_data_v1.csv >donnee_filtree_altitude.csv
+    awk -F';' '{print $10 ";" $1 ";" $6}' meteo_filtered_data_v1.csv>donnee_filtree_temperature_et_num_p.csv
 }
 filtrage_h() {
-    cut -d ';' -f 1,4,15 meteo_filtered_data_v1.csv >donnee_filtree_humidite.csv
+    awk -F';' '{print $10 ";" $1 ";" $4}' meteo_filtered_data_v1.csv>donnee_filtree_humidite.csv
 }
 #execution des arguments et options
 execution_mode_t() {
@@ -70,7 +70,6 @@ execution_mode_p() {
         filtrage_3_bis
     fi
 }
-
 #while true; do
 # Traitement des options de la ligne de commande avec getopt
 while getopts ":t:p:wmhFGSAOQ-:" option; do
@@ -102,9 +101,9 @@ while getopts ":t:p:wmhFGSAOQ-:" option; do
             filtrage_m
             ;; #traitement de l'option humidite
         h)
-            filtrage h
+            filtrage_h
             ;; #traitement de l'option altitude
-        f)  nom_fichier=$OPTARG  ;;
+        f) nom_fichier=$OPTARG ;;
         #option geographique
         F)
             option_geographique="F"
@@ -141,31 +140,56 @@ for f in $fichier_csv; do
     i=$(($i + 1))
     case $option_geographique in
     F)
-        cut -d ';' -f 1- "$f" | grep -E '1[001-976][1-7]' >donnee_filtree_metropole_$i.csv
-        rm $f
+        awk -F";" '{
+  split($1, coords, ",")
+  if (coords[1] + 0 >= 41 && coords[1] + 0<= 51 && coords[2] + 0>= -5 && coords[2] + 0 <= 2) {
+    print $0
+} }' $f >donnee_filtree_metropole_$i.csv
+        #rm $f
         ;;
     G)
-        cut -d ';' -f 1- "$f" | grep -E '973[01-62]' >donnee_filtree_Guyane_$i.csv
-        rm $f
+        awk -F";" '{
+  split($1, coords, ",")
+  if (coords[1] + 0 >= -4 && coords[1] + 0 <= 8  && coords[2]+0 >=-58 && coords[2] + 0<= -48) {
+    print $0
+} }' $f >donnee_filtree_Guyane_$i.csv
+        #rm $f
         ;;
     S)
-        cut -d ';' -f 1- "$f" | grep -E '97500' >donnee_filtree_Saint_Miq_$i.csv
-        rm $f
+        awk -F";" '{
+  split($0, coords, ",")
+  if (coords[1] +0>= 45 && coords[1] + 0 <=50  && coords[2]+0 >= -60 && coords[2] + 0 <= -53) {
+    print $0
+} }' "$f" >donnee_filtree_Saint_Miq_$i.csv
+        #rm $f
         ;;
     A)
-        cut -d ';' -f 1- "$f" | grep -E '971[00-90]' >donnee_filtree_Antille_$i.csv
-        rm $f
+        awk -F";" '{
+  split($0, coords, ",")
+  if (coords[1] +0>= 11 && coords[1] + 0 <=16  && coords[2]+0 >= -65 && coords[2] + 0 <= -57) {
+    print $0
+} }' "$f" >donnee_filtree_Antille_$i.csv
+        #rm $f
         ;;
     O)
-        cut -d ';' -f 1- "$f" | grep -E '974[30-40]' >donnee_filtree_Ocean_$i.csv
-        rm $f
+        awk -F";" '{
+  split($0, coords, ",")
+  if (coords[1] +0>= -26 && coords[1] + 0 <=-10  && coords[2]+0 >= 35 && coords[2] + 0 <= 61) {
+    print $0
+} }' "$f" >donnee_filtree_Ocean_$i.csv
+        #rm $f
         ;;
     Q)
-        cut -d ';' -f 1- "$f" | grep -E '984[11-15]' >donnee_filtree_antarctique_$i.csv
+        awk -F";" '{
+  split($0, coords, ",")
+  if (coords[1] +0>= -75 && coords[1] + 0 <=-41  && coords[2]+0 >= 36 && coords[2] + 0 <= 131) {
+    print $0
+} }' "$f"  "$f" >donnee_filtree_antarctique_$i.csv
         rm $f
         ;;
     esac
 done
 shift $((OPTIND - 1))
-make
+#make
 ./abr $nom_fichier $mode_tri
+i=0
