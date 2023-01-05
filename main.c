e18bd1cfa9d773c1c473bbfca4c80a90a99a01f7
hadjuse <104326150+hadjuse@users.noreply.github.com>
2023-01-05 14:15:05 +0100

    Add files via upload
    option avl à débeugué à cause du segFault

--- main.c
+++ main.c
@@ -1,76 +1,50 @@
 #include "arbre_binaire.h"
 #include <stdio.h>
 #include <stdlib.h>
-int main(){ 
-    //racine
-    pArbre a = creerArbre(1);
-    pArbre pA = a;
-    a = ajouterFilsGauche(a,2);
-    a = ajouterFilsDroit(a, 8);
-    //noeud 2
-    pA = a->fg;
-    pA = ajouterFilsGauche(pA, 3);
-    pA = ajouterFilsDroit(pA, 6);
-    //noeud 3
-    pA = a->fg->fg;
-    pA = ajouterFilsGauche(pA, 4);
-    pA = ajouterFilsDroit(pA, 5);
-
-    //noeud 6
-    pA = a->fg->fd;
-    pA = ajouterFilsDroit(pA,7);
+#include <errno.h>
+#include <regex.h>
+#include <string.h>
+int main(int argc, char **argv)
+{
     
-    //noeud 8
-    pA = a->fd;
-    pA = ajouterFilsDroit(pA, 10);
-    pA = ajouterFilsGauche(pA, 9);
-    parcoursPostFixe(a);
-    //parcoursLargeurs(a);
-    printf("\n");
-    //test suppression
-    pA = a;
-    //modifierRacine(pA, 15);
-    //supprimerFilsGauche(pA);
-    //parcoursLargeurs(a);
-    printf("Nombre de feuille: %d\n", nbmFeuille(pA));
-    printf("Taille: %d\n", taille(pA));
-    printf("Hauteur: %d\n", hauteur(pA));
-    printf("%d\n", recherche(a,10));
-    pArbre ABR = NULL;
-    /*
-    ABR = insertionABRIter(ABR, 2);
-    ABR = insertionABRIter(ABR, 3); 
-    ABR = insertionABRIter(ABR, 1);
-    ABR = insertionABRIter2(ABR, 0);
-    parcoursLargeurs(ABR);*/
-    pArbre pAb = ABR;
-    pAb = insertionABRIter(pAb, 10);
-    pAb = insertionABRIter(pAb, 3);
-    pAb = insertionABRIter(pAb, 5);
-    pAb = insertionABRIter(pAb, 15);
-    pAb = insertionABRIter(pAb, 20);
-    pAb = insertionABRIter(pAb, 12);
-    pAb = insertionABRIter(pAb, 7);
-    pAb = insertionABRIter(pAb, 45);
-    pAb = insertionABRIter(pAb, 9);
-    parcoursLargeurs(pA);
-    printf("moyenne du sous arbre gauche = %f", moyennefg(pAb));
-    //int min,max;
-    //MinMaxABR(pAb, &min,&max);
-    //affArbreGraphique(ABR,5);
-    //free(min);
-    //free(max);
-    pArbre AVL = creerArbre(1);
-    pArbre pAVL = AVL;
-    pAVL = insertionAVL(pAVL, 10, &pAVL->hauteur);
-    pAVL = insertionAVL(pAVL, 30, &pAVL->hauteur);
-    pAVL = insertionAVL(pAVL, 7, &pAVL->hauteur);
-    parcoursInfixe(AVL);
-    pListe liste = creerChainon(5);
-    liste = insertFin(liste,9);
-    liste = insertFin(liste,6);
-    liste = insertFin(liste, 56);
-    printf("\ntaille=%d", taille_liste_chainee(liste));
-
+    //Etape d'ouverture et de lecture du fichier mis en argument
+    if (argc != 3)
+    {
+        fprintf(stderr, "Format : programme <nom du fichier> <mode de tri>");
+        exit(EXIT_FAILURE);
+    }
+    FILE *fichier_a_trier = NULL;
+    fichier_a_trier = fopen(argv[1], "r");
+    if (fichier_a_trier == NULL)
+    {
+        printf("Ouverture du fichier impossible\n");
+        printf("code d'erreur = %d \n", errno);
+        printf("Message d'erreur = %s \n", strerror(errno));
+        exit(1);
+    }
+    char ligne[1024];
+    
+    const char delim[2] = ";";
+    //Cas où le mode tri du script est un avl
+    if (strcmp(argv[2], "avl") == 0){
+        char *token;
+        int c=0;
+        fgets(ligne, sizeof(ligne), fichier_a_trier);
+        int premiere_valeur = atoi(ligne); 
+        pArbre avl = creerArbre(premiere_valeur);
+        pArbre pAvl= avl;
+        while (fgets(ligne, sizeof(ligne), fichier_a_trier))
+        {
+            token = strtok(ligne, delim);
+            token = strtok(NULL, delim);
+            int val = atoi(token);
+            //printf("%d \n", val);
+            pAvl = insertionAVL(pAvl,val,&pAvl->hauteur);
+        }
+        parcoursInfixe(avl,&c);
+        printf("\nNombre de noeuds: %d", c);
+    }
+    fclose(fichier_a_trier);
+    
     return 0;
 }
